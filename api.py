e9d328cf4cd4f16acfe12136dd7b4ef901877239
skippyelvispi <1jackwolf1@gmail.com>
2020-12-19 22:42:20 -0800

    work

--- api.py
+++ api.py
@@ -5,12 +5,16 @@ from dropcam import DropCam
 app = FastAPI()
 cam = DropCam()
 
+@app.get('/dropcam/healthcheck')
+async def dc_healtcheck():
+    return {"status": "healthy!"}
+
 @app.get('/dropcam/file')
 async def dc_file():
     return FileResponse(cam.pic_to_file())
 
 @app.get('/dropcam/stream')
-def dc_stream():
+async def dc_stream():
     stream, ext = cam.pic_to_stream()
     stream.seek(0)
     return StreamingResponse(stream, media_type=ext)
